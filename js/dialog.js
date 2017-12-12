2f69b4d62f69b022a93df3f1003932ef63fe8bab
MixX0 <31200944+MixX0@users.noreply.github.com>
2017-12-12 23:21:15 +0300

    Готово

--- js/dialog.js
+++ js/dialog.js
@@ -18,6 +18,8 @@
   var closePopup = function () {
     setup.classList.add('hidden');
     document.removeEventListener('keydown', onPopupEscPress);
+    setup.style.top = '80px';
+    setup.style.left = '50%';
   };
 
   setupOpen.addEventListener('click', function () {
@@ -48,99 +50,77 @@
 
   // ПЕРЕТАСКИВАНИЕ
 
-  (function () {
-    // var dialogHandle = setup.querySelector('.setup-user-pic');
-    var upload = setup.querySelector('.upload');
-    var shopElement = document.querySelector('.setup-artifacts-shop');
-    var artifactsElement = document.querySelector('.setup-artifacts');
-    var draggedItem = null;
+  var dialogHandle = setup.querySelector('.setup-user-pic');
+  var shopElement = document.querySelector('.setup-artifacts-shop');
+  var artifactsElement = document.querySelector('.setup-artifacts');
+  var draggedItem = null;
 
-    upload.addEventListener('mousedown', function (evt) {
-      evt.preventDefault();
+  dialogHandle.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
 
-      var startCoords = {
-        x: evt.clientX,
-        y: evt.clientY
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
       };
 
-      var onMouseMove = function (moveEvt) {
-        moveEvt.preventDefault();
-
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
-        };
-
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-
-        window.move = {
-          x: shift.x,
-          y: shift.y
-        };
-
-        setup.style.top = (setup.offsetTop - shift.y) + 'px';
-        setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
       };
 
-      var onMouseUp = function (upEvt) {
-        upEvt.preventDefault();
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
 
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-      };
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
 
-      document.addEventListener('mousemove', onMouseMove);
-      document.addEventListener('mouseup', onMouseUp);
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+    };
 
-      setup.style.top = (setup.offsetTop + window.move.y) + 'px';
-      setup.style.left = (setup.offsetLeft + window.move.x) + 'px';
-    });
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
 
-    shopElement.addEventListener('dragstart', function (evt) {
-      if (evt.target.tagName === 'IMG') {
-        draggedItem = evt.target.cloneNode(true);
-      }
-    });
+  shopElement.addEventListener('dragstart', function (evt) {
+    if (evt.target.tagName === 'IMG') {
+      draggedItem = evt.target.cloneNode(true);
+    }
+  });
 
-    artifactsElement.addEventListener('dragover', function (evt) {
-      evt.preventDefault();
-      return false;
-    });
+  artifactsElement.addEventListener('dragover', function (evt) {
+    evt.preventDefault();
+    return false;
+  });
 
-    artifactsElement.addEventListener('drop', function (evt) {
-      evt.target.style.background = '';
-      evt.target.style.borderStyle = '';
-      evt.target.style.borderColor = '';
-      evt.target.style.borderWidth = '';
+  artifactsElement.addEventListener('drop', function (evt) {
+    evt.preventDefault();
 
-      [].slice.call(artifactsElement.children);
+    if (evt.target.tagName !== 'IMG' && evt.target.children.length === 0) {
+      evt.target.appendChild(draggedItem);
+      evt.target.classList.remove('dragSelect');
+      draggedItem = '';
+    }
+  });
 
-      if (artifactsElement.children.indexOf(draggedItem) <= 0) {
-        evt.target.appendChild(draggedItem);
-        evt.preventDefault();
+  artifactsElement.addEventListener('dragenter', function (evt) {
+    if (evt.target.tagName === 'DIV' && evt.target.children.length === 0) {
+      evt.target.classList.add('dragSelect');
+    }
+    evt.preventDefault();
+  });
 
-      } else {
-        evt.target.appendChild(null);
-      }
-    });
-
-    artifactsElement.addEventListener('dragenter', function (evt) {
-      evt.target.style.background = 'yellow';
-      evt.target.style.borderStyle = 'dashed';
-      evt.target.style.borderColor = 'red';
-      evt.target.style.borderWidth = '2px';
-      evt.preventDefault();
-    });
-
-    artifactsElement.addEventListener('dragleave', function (evt) {
-      evt.target.style.background = '';
-      evt.target.style.borderStyle = '';
-      evt.target.style.borderColor = '';
-      evt.target.style.borderWidth = '';
-      evt.preventDefault();
-    });
-  })();
+  artifactsElement.addEventListener('dragleave', function (evt) {
+    evt.target.classList.remove('dragSelect');
+    evt.preventDefault();
+  });
 })();
