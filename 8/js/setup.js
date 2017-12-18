e39445bc7d96594eea09a29ff1b7d5c5979005f0
Keks <keks@htmlacademy.ru>
2017-12-18 14:46:30 +0300

    :heavy_check_mark: Сборка #8

--- 8/js/setup.js
+++ 8/js/setup.js
@@ -6,14 +6,17 @@
   var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
   var QUANTITY_WIZARDS = 4;
+  var FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 
   var setup = document.querySelector('.setup');
   var wizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
   var similarItems = setup.querySelector('.setup-similar');
   var similarPlayer = setup.querySelector('.setup-similar-list');
   var fragment = document.createDocumentFragment();
+  var wizardCoat = setup.querySelector('.setup-wizard .wizard-coat');
+  var wizardEye = setup.querySelector('.setup-wizard .wizard-eyes');
+  var wizardFireball = setup.querySelector('.setup-fireball-wrap');
 
-  // setup.classList.remove('hidden');
   similarItems.classList.remove('hidden');
 
   var getWizardProperty = function () {
@@ -35,15 +38,27 @@
     return wizardElement;
   };
 
+  var fillElement = function (element, color) {
+    element.style.fill = color;
+  };
+
+  var changeElementBackground = function (element, color) {
+    element.style.backgroundColor = color;
+  };
+
   for (var i = 0; i < QUANTITY_WIZARDS; i++) {
     fragment.appendChild(renderWizard(getWizardProperty()));
   }
 
   similarPlayer.appendChild(fragment);
 
+  window.colorizeElement.colorizeElement(wizardCoat, COAT_COLORS[window.utill.isRandomNumberEvent(0, COAT_COLORS.length - 1)], fillElement);
+
+  window.colorizeElement.colorizeElement(wizardEye, EYES_COLORS[window.utill.isRandomNumberEvent(0, EYES_COLORS.length - 1)], fillElement);
+
+  window.colorizeElement.colorizeElement(wizardFireball, FIREBALL_COLORS[window.utill.isRandomNumberEvent(0, FIREBALL_COLORS.length - 1)], changeElementBackground);
+
   window.setup = {
-    setup: setup,
-    COAT_COLORS: COAT_COLORS,
-    EYES_COLORS: EYES_COLORS
+    setup: setup
   };
 })();
